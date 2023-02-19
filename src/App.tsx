558b863079476871cf130f5cfe67681bb7f92d65
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-02-19 12:52:20 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
-import { Box, Stack, Typography } from "@mui/material";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import fr from "./faqs.json";
-import { useMemo } from "react";
-
-interface IContent {
-  question: string;
-  response: string;
-}
-
-interface Item {
-  title: string;
-  content: IContent[];
-}
+import { Box } from "@mui/material";
+import Faqs from "./Faqs";
 
 const App = () => {
-  const items = useMemo((): Item[] => {
-    const keys = Object.keys(fr);
-    return keys.map((key: string) => {
-      return {
-        title: key,
-        // remove key, we just need the contents
-        content: Object.keys(fr[key]).map(
-          (subKey: string) => (fr[key] as any)[subKey]
-        )
-      };
-    });
-  }, []);
-
-  console.log(items);
-
   return (
     <Box
       sx={{
@@ -40,33 +11,7 @@ const App = () => {
         justifyContent: "space-between"
       }}
     >
-      <Stack spacing={4}>
-        {items.map((item, index) => (
-          <Stack key={item.title + index} spacing={2}>
-            <Box>
-              <Typography sx={{ textTransform: "capitalize" }}>
-                {item.title}
-              </Typography>
-            </Box>
-            <Box>
-              {item.content.map((subItem, subIndex) => (
-                <Accordion key={subItem.question + subIndex}>
-                  <AccordionSummary
-                    expandIcon={<button>Cool</button>}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography>{subItem.question}</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <Typography>{subItem.response}</Typography>
-                  </AccordionDetails>
-                </Accordion>
-              ))}
-            </Box>
-          </Stack>
-        ))}
-      </Stack>
+      <Faqs />
     </Box>
   );
 };
---
+++ src/Faqs.tsx
@@ -0,0 +1,65 @@
+import { Box, Stack, Typography } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import fr from "./faqs.json";
+import { useMemo } from "react";
+
+interface IContent {
+  question: string;
+  response: string;
+}
+
+interface Item {
+  title: string;
+  content: IContent[];
+}
+
+const Faqs = () => {
+  const items = useMemo((): Item[] => {
+    const keys = Object.keys(fr);
+    return keys.map((key: string) => {
+      return {
+        title: key,
+        // remove key, we just need the contents
+        content: Object.keys(fr[key]).map(
+          (subKey: string) => (fr[key] as any)[subKey]
+        )
+      };
+    });
+  }, []);
+
+  console.log(items);
+
+  return (
+    <Stack spacing={4}>
+      {items.map((item, index) => (
+        <Stack key={item.title + index} spacing={2}>
+          <Box>
+            <Typography sx={{ textTransform: "capitalize" }}>
+              {item.title}
+            </Typography>
+          </Box>
+          <Box>
+            {item.content.map((subItem, subIndex) => (
+              <Accordion key={subItem.question + subIndex}>
+                <AccordionSummary
+                  expandIcon={<button>Cool</button>}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>{subItem.question}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{subItem.response}</Typography>
+                </AccordionDetails>
+              </Accordion>
+            ))}
+          </Box>
+        </Stack>
+      ))}
+    </Stack>
+  );
+};
+
+export default Faqs;
